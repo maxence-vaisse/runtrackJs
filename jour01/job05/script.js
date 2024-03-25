c8b91418d79385e4203f1ab22bf482e43ac56832
maxence-vaisse <maxence.vaisse@laplateforme.io>
2024-03-25 10:45:51 +0100

    job05 finish

---
+++ jour01/job05/script.js
@@ -0,0 +1,7 @@
+function afficherJoursSemaines() {
+    joursSemaines = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"];
+    for (var i = 0; i < joursSemaines.length; i++) {
+        console.log(joursSemaines[i]);
+    }
+}
+afficherJoursSemaines(); // Affiche les jours de la semaine dans la console
