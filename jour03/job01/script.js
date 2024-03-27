57f9a3b7e386cf2c9a02cd2b960ac2560024e8a2
maxence-vaisse <maxence.vaisse@laplateforme.io>
2024-03-27 09:52:55 +0100

    job01 finish

---
+++ jour03/job01/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $("#button").click(function(){
+        $("article").show();
+    });
+
+    $("#button2").click(function(){
+        $("article").hide();
+    });
+});
