a28ad9af780ff7e5e888552700dad58e91ab502c
maxence-vaisse <maxence.vaisse@laplateforme.io>
2024-04-02 14:33:55 +0200

    Ajout des citations

--- jour06/job02/script.js
+++ jour06/job02/script.js
@@ -12,3 +12,22 @@ document.addEventListener("DOMContentLoaded", function() {
       $('#modalCommanderPapillon').modal('hide');
   });
 });
+
+
+// script.js
+document.getElementById('rebootBtn').addEventListener('click', function() {
+  // Afficher le spinner
+  document.getElementById('spinner').classList.remove('d-none');
+  // Tableau de citations de Blade Runner
+  var citations = [
+    "Toutes ces données seront perdues dans le temps comme des larmes dans la pluie.",
+    "Ce sont des moments qui meurent dans le temps comme des larmes dans la pluie.",
+    "J'ai vu des choses que vous autres humains ne pourriez pas croire."
+    // Ajoutez d'autres citations ici si nécessaire
+  ];
+  // Choisir une citation aléatoire
+  var citationAleatoire = citations[Math.floor(Math.random() * citations.length)];
+  // Remplacer le texte spécifique par la citation aléatoire
+  document.querySelector('.jumbotron p:last-of-type').textContent = citationAleatoire;
+});
+
